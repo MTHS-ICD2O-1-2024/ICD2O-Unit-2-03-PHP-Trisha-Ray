16c7d3cc621705a697188cf9a1b7563e11f09758
default <trisha.ray@stu.ocsb.ca>
2025-03-19 16:56:36 +0000

    template

--- answer.php
+++ answer.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit2-03-HTML-MDL-PHP -->
+<!-- ICS2O-Unit1-08-HTML-MDL -->
 <html lang="en-ca">
   <head>
     <meta charset="utf-8" />
@@ -13,10 +13,10 @@
     />
     <link
       rel="stylesheet"
-      href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css"
+      href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"
     />
     <link rel="stylesheet" href="./css/style.css" />
-    <link
+        <link
       rel="apple-touch-icon"
       sizes="180x180"
       href="./apple-touch-icon.png"
@@ -35,9 +35,9 @@
     />
     <link rel="manifest" href="./site.webmanifest" />
     <title>The XXX program, PHP</title>
-</head>
+  </head>
 
-<body>
+  <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
       <header class="mdl-layout__header">
@@ -46,38 +46,14 @@
         </div>
       </header>
       <main class="mdl-layout__content">
-        <div class="page-content">
+        <div class="right-image">
           <img src="./images/image.png" alt="computer image">
         </div>
-        <div class="page-content">Instruction here...</div>
+        <div class="page-content">The answer is ...</div>
         <br />
-        <!-- Simple Textfield for integers-->
-        <form action="answer.php" method="GET">
-          <div class="mdl-textfield mdl-js-textfield">
-            <!-- input pattern attribute -->
-            <input
-              class="mdl-textfield__input"
-              type="text"
-              pattern="-?[0-9]*(\.[0-9]+)?"
-              id="length"
-            />
-            <!-- mdl-textfield__label -->
-            <label class="mdl-textfield__label" for="demo-input"
-              >Length of ... (mm)</label
-            >
-            <!-- class "mdl-textfield__error" -->
-            <span class="mdl-textfield__error">Input is not a number</span>
-          </div>
-          <br />
-          <!-- Flat button with ripple -->
-          <button
-            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            type="submit"
-          >
-            Calculate
-          </button>
-        </form>
-        <br />
+        <div class="page-content-answer">
+          <a href="./index.php">Return ...</a>
+        </div>
       </main>
     </div>
   </body>
